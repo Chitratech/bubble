b9dd6b6b75dd3c0dbbf3e18b55eb4d001f18bd25
unknown <zivarymostafa78@gamil.com>
2019-10-31 17:51:49 +0330

    code was implemented more readable functional!

--- bubble.c
+++ bubble.c
@@ -1,36 +1,59 @@
-/* Bubble sort code */
- 
 #include <stdio.h>
+
+int getInput(int array[]);
+void swap(int array[], int index1 , int index2);
+void printArray(int arr[] , int len);
+void bubbleSort(int array[], int len);
+
+int main(){
+  int array[100], len;
  
-int main()
-{
-  int array[100], n, c, d, swap;
- 
-  printf("Enter number of elements\n");
-  scanf("%d", &n);
- 
-  printf("Enter %d integers\n", n);
- 
-  for (c = 0; c < n; c++)
-    scanf("%d", &array[c]);
- 
-  for (c = 0 ; c < ( n - 1 ); c++)
-  {
-    for (d = 0 ; d < n - c - 1; d++)
-    {
-      if (array[d] > array[d+1]) /* For decreasing order use < */
-      {
-        swap       = array[d];
-        array[d]   = array[d+1];
-        array[d+1] = swap;
-      }
-    }
-  }
- 
-  printf("Sorted list in ascending order:\n");
- 
-  for ( c = 0 ; c < n ; c++ )
-     printf("%d\n", array[c]);
+  
+  len = getInput(array);
+  bubbleSort(array , len);
+  printArray(array , len);
+  
  
   return 0;
+}
+
+void printArray(int arr[] , int len){
+    int index;
+    printf("Sorted list in ascending order:\n");
+
+    for ( index = 0 ; index < len ; index++ )
+        printf("%d  ", arr[index]);
+}
+
+void bubbleSort(int array[], int len){
+
+    int index , j ;
+
+    for (index = 0 ; index < ( len - 1 ); index++)
+        for (j = 0 ; j < len - index - 1; j++)
+            if (array[j] > array[j+1]) /* For decreasing order use < */
+                swap(array , j, j+1);
+
+}
+
+int getInput(int array[]){
+    int len , index;
+    printf("Enter number of elements\n");
+    scanf("%d", &len);
+
+    printf("Enter %d integers\n", len);
+
+    for (index = 0; index < len ; index++)
+        scanf("%d", &array[index]);
+
+
+    return len;
+    
+}
+
+void swap(int array[] , int index1 , int index2){
+    int temp;
+    temp = array[index1];
+    array[index1]  = array[index2];
+    array[index2] = temp;
 }
