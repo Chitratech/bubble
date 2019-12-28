460fe829389a79d4260e7cbc8b3fc1ac4717e09d
Adarsh Kunwar <adarsh.infinite@gmail.com>
2019-12-28 22:18:56 +0530

    Update bubble.c

--- bubble.c
+++ bubble.c
@@ -6,10 +6,11 @@ int main()
 {
   int array[100], n, c, d, swap;
  
-  printf("Enter number of elements\n");
+  printf("Enter the number of elements between 1 & 101\n");
   scanf("%d", &n);
  while(n<=0 || n>100)
  {
+  printf("The range should be between 1 & 101!\nEnter the threshold again!\n")
   scanf("%d", &n);
  }
   printf("Enter %d integers\n", n);
